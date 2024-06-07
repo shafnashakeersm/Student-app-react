4f48f18f107ca0b54c36988fa8131d1176611076
shafnashakeer <shafnasm20@gmail.com>
2024-06-07 14:24:53 +0530

    completed

---
+++ src/components/AddStudent.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import NavStudent from './NavStudent'
+
+const AddStudent = () => {
+  return (
+    <div>
+        <NavStudent/>
+        <div className="container">
+            <div className="row ">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Name</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Addmission No</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Roll No</label>
+                            <input type="text" className="form-control" />
+                            </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Parent Name</label>
+                        <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <label htmlFor="" className="form-label">College</label>
+                        <textarea name="" id="" className="form-control"></textarea>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">DOB</label>
+                        <input type="date" name="" id="" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">E-mail ID</label>
+                        <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Password</label>
+                        <input type="password" name="" id="" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Conform Password</label>
+                        <input type="password" name="" id="" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <button className="btn btn-success">REGISTER</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AddStudent
---
+++ src/components/StudentViewall.jsx
@@ -0,0 +1,110 @@
+import React from 'react'
+import NavStudent from './NavStudent'
+
+const StudentViewall = () => {
+    return (
+        <div>
+            <NavStudent/>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://media.istockphoto.com/id/1044887096/photo/relaxed-student-looking-at-camera-in-the-street.jpg?s=612x612&w=0&k=20&c=wdyduBsGDVpF4KVyT-NH2GJmq_6idPaVjKESgSvrqdg=" height="250px" class="card-img-top" alt="..."></img>
+
+                                </div>
+                                <div class="card-body">
+                                    <a href="#" class="btn btn-primary">CLICK FOR STUDENT DETAILS</a>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://image1.masterfile.com/getImage/NjMwLTAxODczNzgyZW4uMDAwMDAwMDA=AK6qdb/630-01873782en_Masterfile.jpg" height="250px" class="card-img-top" alt="..."></img>
+
+                                </div>
+                                <div class="card-body">
+                                    <a href="#" class="btn btn-primary">CLICK FOR STUDENT DETAILS</a>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://www.internationalstudent.com/images/home/img-just-getting-started-m2.png" height="250px" class="card-img-top" alt="..."></img>
+
+                                </div>
+                                <div class="card-body">
+                                    <a href="#" class="btn btn-primary">CLICK FOR STUDENT DETAILS</a>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://media.istockphoto.com/id/804392676/photo/female-student-portrait.jpg?s=612x612&w=0&k=20&c=g8XAPTTm_McjTjEa5u30gXowGU-g8siOZU4zxScRPww=" height="250px" class="card-img-top" alt="..."></img>
+
+                                </div>
+                                <div class="card-body">
+                                    <a href="#" class="btn btn-primary">CLICK FOR STUDENT DETAILS</a>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://as2.ftcdn.net/v2/jpg/01/19/66/77/1000_F_119667719_f7KnaGzN2HZcwhaIiGgOc2CqUycnwlIR.jpg" height="250px" class="card-img-top" alt="..."></img>
+
+                                </div>
+                                <div class="card-body">
+                                    <a href="#" class="btn btn-primary">CLICK FOR STUDENT DETAILS</a>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://st4.depositphotos.com/1049680/41944/i/450/depositphotos_419449780-stock-photo-young-african-american-girl-wearing.jpg" height="250px" class="card-img-top" alt="..."></img>
+
+                                </div>
+                                <div class="card-body">
+                                    <a href="#" class="btn btn-primary">CLICK FOR STUDENT DETAILS</a>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://www.shutterstock.com/image-photo/portrait-smiling-young-college-student-260nw-1192615495.jpg" height="250px" class="card-img-top" alt="..."></img>
+
+                                </div>
+                                <div class="card-body">
+                                    <a href="#" class="btn btn-primary">CLICK FOR STUDENT DETAILS</a>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://st2.depositphotos.com/1252160/7770/i/450/depositphotos_77707212-stock-photo-friendly-high-school-student-smiling.jpg" height="250px" class="card-img-top" alt="..."></img>
+
+                                </div>
+                                <div class="card-body">
+                                    <a href="#" class="btn btn-primary">CLICK FOR STUDENT DETAILS</a>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://media.istockphoto.com/id/1301038922/photo/smart-young-mixed-race-student-using-laptop-isolated-over-white-background.jpg?s=612x612&w=0&k=20&c=NIBtPfbgJeY9l09nRRCb9MnxvE4yRCLAtrVgnTunEOQ=" height="250px" class="card-img-top" alt="..."></img>
+
+                                </div>
+                                <div class="card-body">
+                                    <a href="#" class="btn btn-primary">CLICK FOR STUDENT DETAILS</a>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://www.shutterstock.com/image-photo/student-college-university-260nw-302087705.jpg" height="250px" class="card-img-top" alt="..."></img>
+
+                                </div>
+                                <div class="card-body">
+                                    <a href="#" class="btn btn-primary">CLICK FOR STUDENT DETAILS</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default StudentViewall
